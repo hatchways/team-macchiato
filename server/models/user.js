07a8f62bef189184156f5f0019708ef83d0516e3
Daniel Ang <42657409+DangAsian@users.noreply.github.com>
2019-08-19 21:15:19 -0400

    User authentication (#20)
    * Ticket #9 - installed the following node packages: bcryptjs, jsonwebtoken, and express validator
    
    * Ticket #9 added migration to change column
    
    * Ticket #9 - Added registration route that utilizes bcryptjs, jsonwebtoken, and express-validator. The registration route gives back a JWT token that has a payload with te corresponding User information when deciphered.

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,16 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE
-  }, {});
+  const User = sequelize.define(
+    "User",
+    {
+      userName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE
+    },
+    {}
+  );
   User.associate = function(models) {
     // associations can be defined here
   };
