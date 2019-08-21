e6d6824f390fb6a7071258181b4303132d7ceb37
kidist <kidistmulusew@gmail.com>
2019-08-21 15:59:19 -0400

    users route profile/:profileId

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,18 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import { theme } from "./themes/theme";
 import LandingPage from "./pages/Landing";
+import ProfilePage from "./pages/Profile";
+
 
 import "./App.css";
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
+      
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        {/* <Route path="/" component={LandingPage} /> */}
+        <Route path="/profile/:profileId" component={ProfilePage} />
       </BrowserRouter>
     </MuiThemeProvider>
   );
