872e4de38de458398adcfdae41aa5e44e82007a7
Daniel Ang <42657409+DangAsian@users.noreply.github.com>
2019-08-22 19:04:58 -0400

    Project model (#26)
    * created project model
    
    * added associations 1 to Many - A User has many projects whereas a project belongs to one User.
    
    * fixed error from user model
    
    * added foreignKey object to project and user model to ensure that default UserId is overwritten
    
    * Ticket #12 Project Model
    
    * #12 resolve code review comments

---
+++ server/migrations/20190822164125-create-project.js
@@ -0,0 +1,42 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Projects", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      photos: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      desc: {
+        type: Sequelize.STRING
+      },
+      link: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        onDelete: "CASCADE",
+        allowNull: false,
+        references: { model: "Users", key: "id" }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Projects");
+  }
+};
---
+++ server/models/project.js
@@ -0,0 +1,18 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Project = sequelize.define(
+    "Project",
+    {
+      photos: DataTypes.ARRAY(DataTypes.TEXT, { defaultValue: null }),
+      desc: DataTypes.STRING,
+      title: DataTypes.STRING,
+      link: DataTypes.STRING
+    },
+    {}
+  );
+  Project.associate = function(models) {
+    // Project belongs to a User ~
+    Project.belongsTo(models.User, { foreignKey: "userId" });
+  };
+  return Project;
+};
