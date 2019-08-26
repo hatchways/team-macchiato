b4b68b8376663bfd46f748684373e3cad6470eda
Daniel Ang <dan.justin.ang@gmail.com>
2019-08-26 15:42:48 -0400

    added delete functionality to reactDropZone

--- client/src/pages/CreateProjectFormHook.js
+++ client/src/pages/CreateProjectFormHook.js
@@ -9,11 +9,35 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+// List
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import List from "@material-ui/core/List";
+
+//Icons
+import CloseIcon from "@material-ui/icons/Close";
 
 const useStyles = makeStyles(theme => ({
   root: {
     margin: "50px",
     padding: theme.spacing(3, 2)
+  },
+  listItem: {
+    background: "palegreen",
+    margin: "5px 0",
+    borderRadius: "25px"
+  },
+  listItemButton: {
+    cursor: "pointer",
+    background: "red",
+    position: "absolute",
+    top: "0",
+    right: "0",
+    padding: "0 10px",
+    zIndex: "1"
+  },
+  xIcon: {
+    zIndex: "0"
   }
 }));
 
@@ -24,7 +48,8 @@ export default function CreateProjectForm() {
   const [description, setDescription] = React.useState("");
   const [link, setLink] = React.useState("");
   const [imgSrc, setImgSrc] = React.useState(null);
-  const imageMaxSize = 1000000000000;
+  const [files, setFiles] = React.useState([]);
+  const maxSize = 1000000000000;
   // Opens the modal
   const handleClickOpen = () => {
     setOpen(true);
@@ -36,37 +61,43 @@ export default function CreateProjectForm() {
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log("helo");
+    //Fetch Or Axios
+    setOpen(false);
+    setTitle("");
+    setDescription("");
+    setLink("");
+    setFiles([]);
   };
 
-  const handleOnDrop = (acceptedFiles, rejectedFiles) => {
-    console.log(acceptedFiles);
-    console.log("rejected files are", rejectedFiles);
-    if (rejectedFiles && rejectedFiles.length > 0) {
-      const currentRejectedFile = rejectedFiles[0];
-      const currentRejectedFileType = currentRejectedFile.type;
-      const currentRejectedFileSize = currentRejectedFile.size;
-      if (currentRejectedFileSize > imageMaxSize) {
-        alert("this file is too big");
-      }
-    }
+  const onDrop = useCallback(
+    file => {
+      setFiles(files.concat(file));
+    },
+    [files]
+  );
 
-    const currentFile = acceptedFiles[0];
-    const reader = new FileReader();
-    reader.addEventListener(
-      "load",
-      () => {
-        console.log(reader.result);
-        setImgSrc(reader.result);
-      },
-      false
-    );
-    reader.readAsDataURL(currentFile);
+  const deleteFile = e => {
+    const deleteFileName = e.target.id;
+    const newFiles = files.filter(file => file.name != deleteFileName);
+    return setFiles(newFiles);
   };
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    disabled: true
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragReject,
+    acceptedFiles,
+    rejectedFiles
+  } = useDropzone({
+    onDrop,
+    accept: "image/png",
+    minSize: 0,
+    maxSize
   });
+
+  const isFileTooLarge =
+    rejectedFiles.length > 0 && rejectedFiles[0].size > maxSize;
 
   return (
     <div>
@@ -114,29 +145,44 @@ export default function CreateProjectForm() {
             type="link"
             fullWidth
           />
-          <Dropzone
-            onDrop={handleOnDrop}
-            maxSize={imageMaxSize}
-            multiple
-            accept="image/png, image/gif"
-          >
-            {({ getRootProps, getInputProps }) => (
-              <section>
-                <div {...getRootProps()}>
-                  <input {...getInputProps()} />
-                  {imgSrc !== null ? <img src={imgSrc} /> : ""}
-                  <p>Drag 'n' drop some files here, or click to select files</p>
-                </div>
-              </section>
-            )}
-          </Dropzone>
+          {/* React DropZone */}
+          <div>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              {!isDragActive && "Click here or drop a file to upload!"}
+              {isDragActive && !isDragReject && "Drop it like it's hot!"}
+              {isDragReject && "File type not accepted, sorry!"}
+              {isFileTooLarge && (
+                <div className="text-danger mt-2">File is too large.</div>
+              )}
+            </div>
+            <List>
+              {files.map(file => {
+                return (
+                  <ListItem className={classes.listItem}>
+                    {file.name}
+                    <div
+                      className={classes.listItemButton}
+                      onClick={deleteFile}
+                      id={file.name}
+                      value={file}
+                    >
+                      <CloseIcon
+                        className={classes.xIcon}
+                        onClick={deleteFile}
+                        id={file.name}
+                        value={file}
+                      />
+                    </div>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </div>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            Subscribe
+          <Button onClick={onSubmit} color="primary">
+            Submit
           </Button>
         </DialogActions>
       </Dialog>
