66f397c9bfc0c69cbdac50356c052975cacc8aa7
User <allan.hy.yuan@gmail.com>
2019-08-27 17:14:15 -0400

    Created Relationships model
    Added User-relationship table associations

---
+++ server/models/relationship.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Relationship = sequelize.define('Relationship', {
+    user_one_id: DataTypes.INTEGER,
+    user_two_id: DataTypes.INTEGER,
+    status: DataTypes.SMALLINT,
+    action_user_id: DataTypes.INTEGER
+  }, {});
+  Relationship.associate = function(models) {
+    // associations can be defined here
+  };
+  return Relationship;
+};
