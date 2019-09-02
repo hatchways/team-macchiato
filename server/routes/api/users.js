efbfca4e6618c8e37af94addad2282b1b02dfef4
User <allan.hy.yuan@gmail.com>
2019-09-02 16:59:16 -0400

    Route to modify add and remove skills from users
    In /api/users.js for now until we can find a better home for it

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -4,6 +4,7 @@ const User = require("../../models").User;
 const passport = require("passport");
 const Project = require("../../models").Project;
 const Skill = require("../../models").Skill;
+const UserSkill = require("../../models").UserSkill;
 
 // @route   GET /api/users/all
 // @desc    Get all users (include: projects & skill)
@@ -48,3 +49,106 @@ router.put(
       })
    }
 );
+
+//////////////////////////////
+// Skills routes under here //
+//////////////////////////////
+
+// Connects user to skill
+// Authorization required
+// If skill DNE, create it
+
+// @route   POST /api/users/newSkill
+// @desc    Connects user to skill, if skill DNE, create it
+// @body    { skillName }
+// @access  Authorized
+router.post(
+   "/newSkill",
+   passport.authenticate("jwt", { session: false }),
+   async (req, res) => {
+      const user = req.user
+      const { skillName } = req.body
+      try {
+         // Look for skill
+         let skill = await Skill.findOne({
+            where: {
+               skill: skillName
+            }
+         })
+
+         let createUserSkill = (user, skill) => {
+            // Create UserSkill
+            let userId = user.id
+            let skillId = skill.id
+            console.log(`User '${user.name}' has added skill '${skill.skill}'`)
+            UserSkill.create({ userId, skillId }).then(userSkill => res.send(userSkill))
+         }
+
+         // If skill doesn't exist, create it
+         // In this case, no need to check if userSkill already exists
+         if (!skill) {
+            skill = await Skill.create({ skill: skillName })
+            return createUserSkill(req.user, skill)
+         }
+
+         // Skill already exists, so...
+         // Check if userSkill already exists
+         let userSkill = await UserSkill.findOne({
+            where: {
+               userId: user.id,
+               skillId: skill.id
+            }
+         })
+         if (userSkill) {
+            return res.status(500).send({ error: `User already has skill '${skill.skill}'` })
+         }
+
+         return createUserSkill(req.user, skill)
+      } catch (err) {
+         console.log(err)
+         res.status(500).send(err)
+      }
+   }
+)
+
+router.delete(
+   "/removeSkill",
+   passport.authenticate("jwt", { session: false }),
+   async (req, res) => {
+      const userId = req.user.id
+      const { skillName } = req.body
+      try {
+         // Look for skill
+         let skill = await Skill.findOne({
+            where: {
+               skill: skillName
+            }
+         })
+         if (!skill) {
+            return res.status(500).send({ error: `Skill ${skillName} Does Not Exist` })
+         }
+
+         let userSkill = await UserSkill.findOne({
+            where: {
+               userId: userId,
+               skillId: skill.id
+            }
+         })
+         if (!userSkill) {
+            return res.status(500).send({ error: `User does not possess ${skillName}` })
+         }
+
+         // Finally, remove skill
+         // return res.send({ skill: skill, userId: userId })
+         UserSkill.destroy({
+            where: { userId: userId, skillId: skill.id }
+         }).then(numOfDestroyedRows => {
+            console.log("Successfully removed User Skill")
+            res.status(200).send({ removed: numOfDestroyedRows })
+         })
+      } catch (err) {
+         console.log(err)
+         res.status(500).send(err)
+      }
+   }
+)
