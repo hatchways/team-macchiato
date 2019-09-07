7b931af326004d2cbe870db1411d012ac3ae049b
User <allan.hy.yuan@gmail.com>
2019-09-07 19:57:06 -0400

    Wrote function in project.js to upload img to s3

--- server/routes/api/projects.js
+++ server/routes/api/projects.js
@@ -5,22 +5,100 @@ const passport = require("passport");
 const Project = require("../../models").Project;
 const Skill = require("../../models").Skill;
 
+const axios = require('axios')
+
 // Get all projects of a user
 router.get("/:userId", async (req, res) => {
-      try {
-         const userId = req.params.userId
-         Project.findAll({
-            where: {
-               userId: userId
-            }
-         }).then(projs => res.send(projs))
-      } catch(err) {
-         console.log(err)
-         res.status(500).send(err)
-      }
+   try {
+      const userId = req.params.userId
+      Project.findAll({
+         where: {
+            userId: userId
+         }
+      }).then(projs => res.send(projs))
+   } catch (err) {
+      console.log(err)
+      res.status(500).send(err)
    }
+}
 )
 
+// import { signS3 } from './awsController'
+
+const handleUpload = (fileName, fileType, base64) => {
+   var aws = require('aws-sdk')
+   aws.config.update({
+      region: 'us-east-2', // Put your aws region here
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+   })
+   const S3_BUCKET = process.env.S3_BUCKET
+   var s3 = new aws.S3()
+   // var s3Bucket = new aws.S3({ params: { Bucket: S3_BUCKET } })
+   
+   let buf = new Buffer(base64.replace(/^data:image\/\w+;base64,/, ""), 'base64')
+
+   // var data = {
+   //    Key: fileName,
+   //    Body: buf,
+   //    ContentEncoding: 'base64',
+   //    ContentType: fileType,
+   // };
+
+   // s3Bucket.putObject(data, function (err, data) {
+   //    if (err) {
+   //       console.log(err);
+   //       console.log('Error uploading data: ', data);
+   //    } else {
+   //       console.log('succesfully uploaded the image!');
+   //    }
+   // });
+
+   ////////////////////////////
+   // WIP
+   // Implement this later ////
+   // bucketName var below crates a "folder" for each user
+   var bucketName = 'myApp/' + req.body.userEmail;
+   
+   var params = {
+      Bucket: S3_BUCKET,
+      Key: fileName,
+      Body: buf,
+      ContentEncoding: 'base64', // might not be necssary
+      ContentType: fileType,
+      ACL: 'public-read'
+   };
+   s3.upload(params, function (err, data) {
+      if (err) return res.status(500).send(err);
+
+      // TODO: save data to mongo
+      res.json(data);
+   });
+   //
+   // Attempting to get a signed request then upload base 64 (Not Working)
+   //
+
+   // signS3(fileName, fileType).then(res => {
+   //    const { signedRequest, url } = res.data.returnData
+   //    console.log(signedRequest)
+   //    console.log(url)
+
+   //    const options = {
+   //       headers: {
+   //          'Content-Type': fileType
+   //       }
+   //    }
+   //    let imageData = base64.replace(/^data:image\/\w+;base64,/, "")
+
+   //    axios.put(signedRequest, imageData, options)
+   //       .then(res => {
+   //          console.log(res)
+   //       })
+   //       .catch(err => {
+   //          console.log(err)
+   //       })
+   // })
+}
 // Upload new project
 router.post(
    "/",
@@ -30,14 +108,25 @@ router.post(
          const userId = req.user.id;
          // console.log(req.body)
          const { photos, title, desc, link } = req.body;
-         console.log(photos)
-         console.log(title)
-         console.log(desc)
-         console.log(link)
+
          // If any are undefined
-         if(!photos || !title || !desc || !link)
-            return res.status(500).send("Sorry, all fields required")
-         // TODO
+         // if (!photos || !title || !desc || !link)
+         //    return res.status(500).send("Sorry, all fields required")
+
+         //////////
+         // TODO //
+         //////////
+
+         let fileName = 'myFileName2.png'
+         let fileType = 'image/png'
+         // Get signed response from s3
+         handleUpload(fileName, fileType, photos[0])
+
+         // signS3('fileName', 'image/png').then(res => {
+         //    console.log(res.data)
+         // })
+
+
          // Do stuff with photos to upload them to s3
          // Create photos array of s3 links
          // let photoS3 = photos.split(',');
@@ -47,8 +136,8 @@ router.post(
             photoS3, title, desc, link, userId
          };
 
-         console.log(project)
-         
+         // console.log(project)
+
          res.send("Yes")
          // Possibly check if project already exists?
          // - using title and link maybe                 LOW PRIO
@@ -57,7 +146,7 @@ router.post(
          //    console.log(`Successfully created project with projId ${proj.id}`);
          //    return res.send(proj);
          // })
-      } catch(err) {
+      } catch (err) {
          console.log(err)
          res.status(500).send(err)
       }
