e92aba95275ecc387b4f4991011e9e0f826eb679
User <allan.hy.yuan@gmail.com>
2019-09-10 14:19:08 -0400

    Rebased to dev

--- server/models/entity.js
+++ server/models/entity.js
@@ -5,22 +5,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Entity.associate = function(models) {
     // associations can be defined here
-<<<<<<< HEAD
-<<<<<<< HEAD
     Entity.belongsToMany(models.User, {
       through: models.Liked_Entity,
       as: "liked_entity",
       foreignKey: "entity_id"
     });
-=======
->>>>>>> #43
-=======
-    Entity.belongsToMany(models.User, {
-      throught: models.Liked_Entity,
-      as: "liked_entity",
-      foreignKey: "entity_id"
-    });
->>>>>>> Refactored some db col naming:
   };
   return Entity;
 };
