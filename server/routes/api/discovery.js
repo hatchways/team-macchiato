4b3e13a54551737a4293389fe61616caff72decb
Daniel Ang <dan.justin.ang@gmail.com>
2019-09-10 12:53:55 -0400

    completed back end to serve all users & to take into account of name params for now

--- server/routes/api/discovery.js
+++ server/routes/api/discovery.js
@@ -1,13 +1,42 @@
 const express = require("express");
 const router = express.Router();
+// var pg = require("pg");
 const User = require("../../models").User;
-const User = require("../../models").Project;
+const Project = require("../../models").Project;
+
+// Helper function (will place somewhere else)
+const isEmptyObj = object => !Object.getOwnPropertySymbols(object).length && !Object.getOwnPropertyNames(object).length
+
+
+// @route   GET /api/discovery
+// @desc    Route that fetches all Users(n=...)within the platform 
+// @access  Public
 
 router.get('/', async (req, res) => {
-  User.findAll({
-    include: [{model: Project}]
-  }).then(users => {
+ const { name } = req.query;
+ console.log(req.query)
+ console.log(isEmptyObj(req.query))
+  try {
 
-  })
+    if (req.query) {
+      User.findAll({
+        where: {
+          name: name
+        }
+      }).then(users => {
+        return res.json(users)
+      })
+    }
+
+    User.findAll({
+      include: [{all: true}]
+    }).then(users => {
+      return res.json(users)
+    })
+    // SELECT * FROM Users WHERE name = "Daniel"
+  } catch (err) {
+    console.log(err)
+  }
 })
+
 module.exports = router
