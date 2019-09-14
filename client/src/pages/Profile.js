5f10b620a80fd6527a0260b40cb64ba60002b24f
kidist <kidistmulusew@gmail.com>
2019-09-14 15:21:56 -0400

    project detail when clicked
    project detail when clicked

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Link } from "react-router-dom";
-
+import { Modal, Form, Button } from "react-bootstrap";
+import {ProjectDetail} from './ProjectDetail';
 import Dropzone from 'react-dropzone';
 
 import Nav from "./Nav";
@@ -8,90 +9,123 @@ import img1 from '../assets/images/creativedesignerediting34199.jpg';
 import img6 from '../assets/images/profilepic.jpg';
 
 class ProfilePage extends Component {
-   render() {
-      return (
-         <div>
-            <Nav />
-            <div className="row">
-               {/* <div className="gif-container"> */}
-               <div className=" col-lg-3 profile-detail ">
-                  <Dropzone onDrop={this.onDrop} accept="image/jpg, image/png">
-                     {({ getRootProps, getInputProps, isDragActive }) => (
-                        <div {...getRootProps()}>
-                           <input {...getInputProps()} />
-                           <img className='profile-image' src={img6} />
+       
+       constructor(props){
+              super(props);
+              this.state={addModalshow:false}
+          }
 
-                        </div>
-                     )}
-                  </Dropzone>
-
-
-
-                  <h5 className='name-text'>David Rommel</h5>
-                  <h5 className='title-text'>Designer & Creative Art Director</h5>
-                  <h5 className='city-text'>Toronto Canada</h5>
-                  <button className='orange-button hire-button'>Hire me</button>
-                  <button className='white-button'>Message</button>
-                  <hr />
-                  <h6 className='skill-title'>Skills: </h6>
-                  <div className="row ">
-                     <div className="col-lg-3 ">
-                        <button className='skill-box'>DESIGN</button>
-                     </div>
-                     <div className="col-lg-3 ">
-                        <button className='skill-box'>ILLUSTRATION</button>
-                     </div>
-                     <div className="col-lg-2 ">
-                        <button className='skill-box skill-UI'>UI</button>
-                     </div>
-                     <div className="col-lg-2 ">
-                        <button className='skill-box'>UX</button>
-                     </div>
-                     <div className=" skill-row row ">
-                        <div className="col-lg-6 ">
-                           <button className='skill-box skill-product'>PRODUCT DESIGN</button>
-                        </div>
-                        <div className="col-lg-4 ">
-                           <button className='skill-box'>MOBILE</button>
-                        </div>
-                     </div>
-                  </div>
-                  <hr />
-                  <h6 className='about-title'>About</h6>
-                  <p className='about-text'>I am a create designer from Toronto.I enjoy creating solutions for web anad mobile app.
+  render() {
+       let addModalclose=()=>this.setState({addModalShow:false});
+    return (
+      <div>
+      <Nav/>
+      <div className="row">   
+                {/* <div className="gif-container"> */}
+                <div  className=" col-lg-3 profile-detail "> 
+                <Dropzone onDrop={this.onDrop} accept="image/jpg, image/gif">
+                    {({getRootProps, getInputProps, isDragActive}) => (
+                     <div {...getRootProps()}>
+                         <input {...getInputProps()} />
+                             <img className='profile-image' src={img6} />
+      
+                      </div>
+                             )}
+                </Dropzone>
+       
+        
+                
+                    <h5 className='name-text'>David Rommel</h5>
+                    <h5 className='title-text'>Designer & Creative Art Director</h5>
+                    <h5 className='city-text'>Toronto Canada</h5>
+                    <button className='orange-button hire-button'>Hire me</button>
+                    <button className='white-button'>Message</button>
+                    <hr className='hr-prof'/>
+                    <h6 className ='skill-title'>Skills: </h6>
+                    <div className="row "> 
+                            <div className="col-lg-3 ">
+                                   <button className ='skill-box'>DESIGN</button>
+                            </div>
+                            <div className="col-lg-3 ">
+                                 <button className ='skill-box'>ILLUSTRATION</button>
+                            </div>
+                            <div className="col-lg-2 ">
+                                 <button className ='skill-box skill-UI'>UI</button>
+                            </div>
+                            <div className="col-lg-2 ">
+                                   <button className ='skill-box'>UX</button>
+                            </div>
+                            <div className=" skill-row row "> 
+                                  <div className="col-lg-6 ">
+                                        <button className ='skill-box skill-product'>PRODUCT DESIGN</button>
+                                  </div>
+                                  <div className="col-lg-4 ">
+                                          <button className ='skill-box'>MOBILE</button>
+                                    </div>
+                             </div>
+                      </div>
+                      <hr className='hr-prof'/>
+                       <h6 className ='about-title'>About</h6>
+                       <p className ='about-text'>I am a create designer from Toronto.I enjoy creating solutions for web anad mobile app.
                         Available  for fulltime, freelance or remote work opportunities.</p>
-               </div>
-               <div className="col-lg-7 verticalLine ">
-                  <div className="row last-row">
-                     <div className="col-lg-5 ">
+                 </div>
+               <div className="col-lg-7 verticalLine "> 
+                   <div className="row last-row"> 
+                         <div className="col-lg-5 ">
+                    
+                               <Button  className='project-button' variant="primary" onClick={()=>this.setState({addModalShow:true})}>
+                                      <img className='project-image' src= {img1} />
+                               </Button>   
 
-                        <img className='project-image' src={img1} />
-                     </div>
+                                <ProjectDetail show={this.state.addModalShow}
+                                 onHide={addModalclose}/>
 
-                     <div className="col-lg-5  ">
-
-                        <img className='project-image' src={img1} />
-                     </div>
-
+                           </div>
+                       <div className="col-lg-5  ">
+                    
+                                  <Button  className='project-button' variant="primary" onClick={()=>this.setState({addModalShow:true})}>
+                      
+                                        <img className='project-image' src= {img1} />
+                                   </Button>   
+                                       <ProjectDetail show={this.state.addModalShow}
+                                        onHide={addModalclose}/>
+                       </div>
+                </div>
+                    <div className="row"> 
+                          <div className="row  middle-row "> 
+                                   <div className="col-lg-5  ">
+                    
+                                         <Button  className='project-button' variant="primary" onClick={()=>this.setState({addModalShow:true})}>
+                      
+                                              <img className='project-image' src= {img1} />
+          
+                                         </Button>   
+                                          <ProjectDetail show={this.state.addModalShow}
+                                            onHide={addModalclose}/>
+                                  </div>
+                                  <div className="col-lg-5  ">
+                                         <Button  className='project-button' variant="primary" onClick={()=>this.setState({addModalShow:true})}>
+                                                <img className='project-image' src= {img1} />
+                                          </Button>  
+                                         <ProjectDetail show={this.state.addModalShow}
+                                         onHide={addModalclose}/>
+                                 </div>
+                           </div>
                   </div>
-                  <div className="row">
-                     <div className="col-lg-5  ">
+                  <div className="row"> 
+                         <div className="col-lg-5 ">
+                    
+                                 <Button  className='project-button' variant="primary" onClick={()=>this.setState({addModalShow:true})}>
+                      
+                                     <img className='project-image' src= {img1} />
+          
+                                 </Button>   
 
-                        <img className='project-image' src={img1} />
-                     </div>
-                     <div className="col-lg-5  ">
-
-                        <img className='project-image' src={img1} />
-                     </div>
-
-                  </div>
-                  <div className="row">
-                     <div className="col-lg-5 ">
-
-                        <img className='project-image' src={img1} />
-                     </div>
-
-                  </div>
+                                    <ProjectDetail show={this.state.addModalShow}
+                                         onHide={addModalclose}/>
+                          </div>
+                    
+                    </div>
                </div>
             </div>
          </div>
